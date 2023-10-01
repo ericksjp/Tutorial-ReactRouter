610d38dd9347b66d67ece4d9f586e5f411ad7053
Erick <ericksjp703@gmail.com>
2023-10-01 02:10:13 -0300

    Parametros de pesquisa

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,10 +1,12 @@
-import './styles.css'
-import {NavLink, Outlet } from "react-router-dom";
+import "./styles.css";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 export default function Invoices() {
   // eslint-disable-next-line prefer-const
   let invoices = getInvoices();
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: "flex" }}>
@@ -14,17 +16,41 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            className={({isActive}) => isActive ? "dblock nav-red" : "dblock nav-blue"}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          type="text"
+          value={searchParams.get("name") || ""}
+          onChange={(event) => {
+            const name = event.target.value;
+            if (name) {
+              setSearchParams({ name });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const name = searchParams.get("name");
+            if (!name) {
+              return true;
+            }
+            const invoiceName = invoice.name.toLowerCase();
+            return invoiceName.includes(name.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "dblock nav-red" : "dblock nav-blue"
+              }
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
